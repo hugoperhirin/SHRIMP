4de7f4f97cda397efd570cf81212a458e604d0c2
hugo perhirin <hugodoggydogg@gmail.com>
2024-02-05 17:21:28 +0100

    Opened and edited subgoals + reserve, few css tweaks, tied up some folders

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', throttle(parallax, 13));
 
 function throttle(fn, wait) {
   let time = Date.now();
-  return function() {
+  return function () {
     if ((time + wait - Date.now()) < 0) {
       fn();
       time = Date.now();
@@ -14,39 +14,41 @@ function throttle(fn, wait) {
 };
 
 function parallax() {
-	let scrolled = window.pageYOffset;
-	
-	let parallax = document.querySelector("#level0");
-	parallax.style.transform = 'translateY(' + (scrolled * 0.8) + 'px' + ')';
-	
-	let parallax1 = document.querySelector("#level1");
-	parallax1.style.transform = 'translateY(' + (scrolled * 0.6) + 'px' + ')';
+  let scrolled = window.pageYOffset;
 
-	let parallax2 = document.querySelector("#level2");
-	parallax2.style.transform = 'translateY(' + (scrolled * 0.3) + 'px' + ')';
-	
-	let parallax3 = document.querySelector(".logo");
-	parallax3.style.transform = 'translateY(' + (-scrolled * 0.8) + 'px' + ')';
+  let parallax = document.querySelector("#level0");
+  parallax.style.transform = 'translateY(' + (scrolled * 0.8) + 'px' + ')';
+
+  let parallax1 = document.querySelector("#level1");
+  parallax1.style.transform = 'translateY(' + (scrolled * 0.6) + 'px' + ')';
+
+  let parallax2 = document.querySelector("#level2");
+  parallax2.style.transform = 'translateY(' + (scrolled * 0.3) + 'px' + ')';
+
+  let parallax3 = document.querySelector(".logo");
+  parallax3.style.transform = 'translateY(' + (-scrolled * 0.8) + 'px' + ')';
 };
 // Toggle cursor
 let cursorEnabled = false;
 
-document.getElementById('cursorToggle').addEventListener('click', function() {
-    if(cursorEnabled) {
-        // Set back to default cursor
-        document.body.style.cursor = 'auto';
-        setLinksCursor('auto');
-        cursorEnabled = false;
-    } else {
-        // Set custom cursors
-        document.body.style.cursor = 'url(\'/root/cursornormal.png\'), auto';
-        setLinksCursor('url(\'/root/cursorgolden.png\'), pointer');
-        cursorEnabled = true;
-    }
+document.getElementById('cursorToggle').addEventListener('click', function () {
+  if (cursorEnabled) {
+    // Set back to default cursor
+    document.body.style.cursor = 'auto';
+    setLinksCursor('auto');
+    cursorEnabled = false;
+  } else {
+    // Set custom cursors
+    document.body.style.cursor = 'url(\'/root/cursornormal.png\'), auto';
+    setLinksCursor('url(\'/root/cursorgolden.png\'), pointer');
+    cursorEnabled = true;
+  }
 });
 
+
+
 function setLinksCursor(cursorStyle) {
-    document.querySelectorAll('a > img, .navbar > ul > li > a').forEach(el => {
-        el.style.cursor = cursorStyle;
-    });
+  document.querySelectorAll('a > img, .navbar > ul > li > a').forEach(el => {
+    el.style.cursor = cursorStyle;
+  });
 }
